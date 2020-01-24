018a9c23207ff6e7823adf5a627541b669de7a99
Florent Chuffart <florent.chuffart@gmail.com>
2020-01-24 22:07:49 +0100

    Data preprocessing

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Tissue restricted genes data frame 
+#'
+#' The `ector_tissue_restricted_genes` data frame describes tissue-restricted genes. 
+#' The rows correspond to genes while the columns correspond to their related informations, 
+#' including position in GRCh38 reference genome.
+#' The data presentation is inspired by BED (Browser Extensible Data) format.
+#' Tissue restricted genes are determined using threshold algorithme [ref. EpiMed]. ons.
+#'
+#' @format A data frame with 3433 rows and 7 columns
+#' @source \url{http://epimed.univ-grenoble-alpes.fr/database}
+"ector_tissue_restricted_genes"
+
+#' Transcriptome matrix
+#'
+#' The ector_transcriptome matrix...
+#'
+#' @format A matrix with 3000 rows and 44 columns
+#' @source \url{https://portal.gdc.cancer.gov}
+"ector_transcriptome"
+
+#' Clinicals data frame
+#'
+#' The `ector_clinicals` data frame describes clinical information. 
+#'
+#' @format A data frame with XXX rows and 7 columns
+#' @source \url{https://portal.gdc.cancer.gov}
+"ector_clinicals"
