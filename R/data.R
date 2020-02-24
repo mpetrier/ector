b38ac34af2d90191d8fe85cff72e61bb190e974b
Florent Chuffart <florent.chuffart@gmail.com>
2020-02-24 15:59:47 +0100

    Adding Rbuildignore et refactoring.

--- R/data.R
+++ R/data.R
@@ -1,14 +1,14 @@
-#' Tissue restricted genes data frame 
+#' Tissue specific genes data frame 
 #'
-#' The `ector_tissue_restricted_genes` data frame describes tissue-restricted genes. 
+#' The `ector_tissue_specific_genes` data frame describes tissue-specific genes. 
 #' The rows correspond to genes while the columns correspond to their related informations, 
 #' including position in GRCh38 reference genome.
 #' The data presentation is inspired by BED (Browser Extensible Data) format.
-#' Tissue restricted genes are determined using threshold algorithme [ref. EpiMed]. ons.
+#' Tissue specific genes are determined using threshold algorithme [ref. EpiMed]. ons.
 #'
 #' @format A data frame with 3433 rows and 7 columns
 #' @source \url{http://epimed.univ-grenoble-alpes.fr/database}
-"ector_tissue_restricted_genes"
+"ector_tissue_specific_genes"
 
 #' Transcriptome matrix
 #'
